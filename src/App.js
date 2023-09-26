94fb3997b2632ed1746a3286e80641b07a6a7634
Mohamed-code-ship <mohaabdul568@gmail.com>
2023-09-26 16:10:36 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
+import { SiSubstack } from 'react-icons/si';
 import './App.css';
+import About from './components/about/About';
+import Developer from './components/developer/Developer';
+import Hero from './components/hero/Hero';
+import Navbar from './components/navbar/Navbar';
+import Subscribe from './components/subscribe/Subscribe';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+          <Navbar/>
+          <Hero/>
+          <About/>
+          <Developer/>
+          <Subscribe/>
+          
     </div>
   );
 }
